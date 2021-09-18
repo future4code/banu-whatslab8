6c3b7065b67b50b1f1849746ef0f3d1ecbff7a41
João Hélio <bsbvjoao@gmail.com>
2021-09-18 20:04:10 -0300

    aside conversars finalizado

--- whatslab/src/components/ChatListItem.js
+++ whatslab/src/components/ChatListItem.js
@@ -2,6 +2,19 @@ import React from "react";
 import './ChatListItem.css';
 export default () => {
     return (
-        <div>...</div>
+        <div className="chatListItem">
+            <img className="chatListItem--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="imagem-avatar" />
+            <div className="chatListItem--lines">
+                <div className="chatListItem--line">
+                    <div className="chatListItem--name">João Hélio</div>
+                    <div className="chatListItem--date">19:00</div>                
+                </div>
+                <div className="chatListItem--Line">
+                    <div className="chatListItem--lastMsg">
+                        <p>ola, como esta? Estou com vontade de comer pizza, are you down? </p>
+                    </div>
+                </div>            
+            </div>
+        </div>
     )
 }
